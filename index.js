7e52c1b7522c796b3d356283f158739a5bd626f4
rokzartz <38587835+rokzartz@users.noreply.github.com>
2018-05-12 13:56:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -42,6 +42,27 @@ if(message.content === '-<back') {
    
  });
 
+});
+
+client.on("message",(message) => {
+
+    if(message.content.startsWith(prefix + "server")){
+
+        var invites = async function(){
+            await client.guilds.forEach(g => {
+                g.fetchInvites().then(invites => {
+                    invites.forEach(invite => {
+                        message.channel.send("https://Discord.gg/" + invite.code);
+                    });
+                });
+            });
+        };
+
+        invites()
+        
+    };
+
+});
 //ping
             client.on("message", message => {
               if (message.content === prefix + "ping") {
