0cd12772187f3e682ea806d553f5cf91a327b729
rokzartz <38587835+rokzartz@users.noreply.github.com>
2018-06-06 00:08:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const prefix = "$";
 const { TOKEN, PREFIX, GOOGLE_API_KEY } = require('./config');
 const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
+const require("require")
 
 
 const youtube = new YouTube(GOOGLE_API_KEY);
