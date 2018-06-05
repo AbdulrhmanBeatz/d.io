f43ee0e2e7502ae59e006a3cfffc6ebacf01a232
rokzartz <38587835+rokzartz@users.noreply.github.com>
2018-06-05 23:34:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const client = new Client({ disableEveryone: true });
+const Discord = require("discord.js")
 const client = new Discord.Client();  
 const fs = require("fs")
 const { TOKEN, PREFIX, GOOGLE_API_KEY } = require('./config');
