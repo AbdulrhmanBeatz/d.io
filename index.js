ae6811d1c73cca54d6a5f35d7e66c11ec4a6a884
rokzartz <38587835+rokzartz@users.noreply.github.com>
2018-06-06 01:57:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1399,7 +1399,7 @@ client.on('guildMemberRemove', member => {
           
 client.on('guildMemberAdd', member => {
     const guild = member.guild;
-    const channel = member.guild.channels.find('name', 'welcome');
+    const channel = member.guild.channels.find('name', 'wèĺčómĕ');
     if (!channel) return;
     let memberavatar = member.user.avatarURL
     const fromNow = moment(member.joinedTimestamp).fromNow();
@@ -1415,7 +1415,7 @@ client.on('guildMemberAdd', member => {
 
 client.on('guildMemberAdd', member => {
 
-const channel = member.guild.channels.find("name","welcome")
+const channel = member.guild.channels.find("name","wèĺčómĕ")
 if (member.user.bot) return;
 var Canvas = require('canvas')
 var jimp = require('jimp')
@@ -2397,80 +2397,6 @@ message.channel.send(':ballot_box_with_check:**Your info was changed to \`' + ar
 }
 }
 )
-
-
-client.on('guildMemberAdd', member => {
-    let channel = member.guild.channels.find("name", "wèĺčómĕ")
-
-    
-        if (member.user.bot) return;
-const w = ['./image.png'];
-        let Image = Canvas.Image,
-            canvas = new Canvas(749, 198),
-            ctx = canvas.getContext('2d');
-        ctx.patternQuality = 'bilinear';
-        ctx.filter = 'bilinear';
-        ctx.antialias = 'subpixel';
-        ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
-        ctx.shadowOffsetY = 2;
-        ctx.shadowBlur = 2;
-        ctx.stroke();
-        ctx.beginPath();
-
-        fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
-            if (err) return console.log(err);
-            let BG = Canvas.Image;
-            let ground = new Image;
-            ground.src = Background;
-            ctx.drawImage(ground, 0, 0, 749, 198);
-
-})
-
-                let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".png" : member.user.displayAvatarURL;
-                jimp.read(url, (err, ava) => {
-                    if (err) return console.log(err);
-                    ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
-                 if (err) return console.log(err);
-
-ctx.font = '35px Aeland';
-                        ctx.fontSize = '40px';
-                        ctx.fillStyle = "#FFFFFF";
-                        ctx.textAlign = "center";
-                        ctx.fillText(" Welcome to " + member.guild.name , 440, 25);
-
-                        //ur name
-                        ctx.font = '40px Impact';
-                        ctx.fontSize = '48px';
-                        ctx.fillStyle = "#FFFFFF";
-                        ctx.textAlign = "center";
-                        ctx.fillText(member.user.username, 420, 100);
-
-                         ctx.font = '30px Impact';
-                        ctx.fontSize = '20px';
-                        ctx.fillStyle = "#FFFFFF";
-                        ctx.textAlign = "center";
-                        ctx.fillText("You are member No." + member.guild.memberCount, 410, 170);
-
-
-                        //Avatar
-                        let Avatar = Canvas.Image;
-                              let ava = new Avatar;
-                              ava.src = buf;
-                              ctx.beginPath();
-                              ctx.arc(115, 100, 90, 0, Math.PI*2);
-                                 ctx.closePath();
-                                 ctx.clip();
-                                 ctx.drawImage(ava, 5, 5, 200, 200);
-                                 channel.sendMessage(`<@${member.user.id}>`)
-        channel.sendFile(canvas.toBuffer())
-
-
-
-})
-})
-
-})
-
 
 
 client.login(process.env.BOT_TOKEN)
